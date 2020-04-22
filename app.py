0c12ec55ddf1cf9565a082ecb2485d631c0c749d
Ryan <rb@determined.ai>
2020-04-22 00:57:30 -0600

    Add better interfaces.
    Prepare to manage more processes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import curses
+import abc
 import enum
 import time
 import sys
@@ -60,26 +60,67 @@ MASTER_CMD = [
     "--root", os.path.expanduser("~/code/determined/build/share/determined/master"),
 ]
 
-# STATES:
-#
-# DOWN
-# PRE_DB
-# DB
-# PRE_HASURA
-# HASURA
-# PRE_MASTER
-# MASTER
-# AGENT
+AGENT_CMD = [
+    os.path.join(os.path.expanduser("~/go/bin"), "determined-agent"),
+    "--master_host", "192.168.0.4",
+    "--master_port", "8080",
+    "run"
+]
+
 
 class States(enum.Enum):
     DEAD = "DEAD"
-    PRE_DB = "PRE_DB"
     DB = "DB"
-    PRE_HASURA = "PRE_HASURA"
     HASURA = "HASURA"
-    PRE_MASTER = "PRE_MASTER"
     MASTER = "MASTER"
     AGENT = "AGENT"
+
+    def __str__(self):
+        if self == States.DEAD: return "DEAD"
+        if self == States.DB: return "DB"
+        if self == States.HASURA: return "HASURA"
+        if self == States.MASTER: return "MASTER"
+        if self == States.AGENT: return "AGENT"
+
+    def get_index(self):
+        return {
+            States.DEAD: 0,
+            States.DB: 1,
+            States.HASURA: 2,
+            States.MASTER: 3,
+            States.AGENT: 4,
+        }[self]
+
+    def __cmp__(self, other):
+        s = self.get_index()
+        o = other.get_index()
+        if s < o: return -1
+        return int(s > o)
+
+    def __lt__(self, other):
+        return self.get_index() < other.get_index()
+
+    @staticmethod
+    def from_index(index):
+        return (
+            States.DEAD,
+            States.DB,
+            States.HASURA,
+            States.MASTER,
+            States.AGENT,
+        )[min(max(index ,0), 4)]
+
+    def __sub__(self, num):
+        return States.from_index(self.get_index() - num)
+
+    def __add__(self, num):
+        return States.from_index(self.get_index() - num)
+
+    def __rsub__(self, num):
+        return States.from_index(self.get_index() - num)
+
+    def __radd__(self, num):
+        return States.from_index(self.get_index() - num)
 
 
 def asbytes(msg):
@@ -88,7 +129,179 @@ def asbytes(msg):
     return msg.encode('utf8')
 
 
+def nonblock(fd):
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+class AtomicOperation:
+    """
+    Only have one atomic operation in flight at a time.  You must wait for it to finish but you may
+    request it ends early if you know you will ignore its output.
+
+    An example would be a connector which is trying to connect to the master binary, except if the
+    master binary has already exited, we will want to exit the connector.
+    """
+
+    @abc.abstractmethod
+    def __str__(self):
+        """Return a one-word summary of what the operation is"""
+        pass
+
+    @abc.abstractmethod
+    def cancel(self):
+        pass
+
+    @abc.abstractmethod
+    def join(self):
+        pass
+
+class Process:
+    """
+    A long-running process may have precommands to run first and postcommands before it is ready.
+    """
+
+    @abc.abstractmethod
+    def get_precommand(self):
+        """Return the next AtomicOperation or None, at which point it is safe to run_command)."""
+        pass
+
+    @abc.abstractmethod
+    def get_postcommand(self):
+        """Return the next AtomicOperation or None, at which point the command is up."""
+        pass
+
+    @abc.abstractmethod
+    def run_command(self):
+        """Start the long-running process."""
+        pass
+
+    @abc.abstractmethod
+    def running(self):
+        """Is the long-running process still un-waited?"""
+        pass
+
+    @abc.abstractmethod
+    def kill(self):
+        """Kill the long-running process (atomic operations must be canceled normally)."""
+        pass
+
+class Master(Process):
+    def __init__(self, poll, log, pipe_wr, set_target, next_thing):
+        self.proc = None
+        self.out = None
+        self.err = None
+        self.dying = False
+
+        self.poll = poll
+        self.pipe_wr = pipe_wr
+        self.log = log
+        self.next_thing = next_thing
+        self.set_target = set_target
+
+        self._reset()
+
+    def _reset(self):
+        self.postcommands_run = 0
+
+    def _maybe_wait(self):
+        """wait() on master if both stdout and stderr are empty."""
+        if not self.dying:
+            self.log(f"master closing unexpectedly!\n")
+            self.set_target(States.DEAD)
+
+        if self.out is None and self.err is None:
+            ret = self.proc.wait()
+            self.log(f"master exited with {ret}\n")
+            self.log(f"NDET: master exited with {ret}\n", "master")
+            self.proc = None
+            self._reset()
+            self.next_thing()
+
+    def _handle_out(self, ev):
+        if ev & Poll.IN_FLAGS:
+            self.log(os.read(self.out, 4096), "master")
+        if ev & Poll.ERR_FLAGS:
+            self.poll.unregister(self._handle_out)
+            os.close(self.out)
+            self.out = None
+            self._maybe_wait()
+
+    def _handle_err(self, ev):
+        if ev & Poll.IN_FLAGS:
+            self.log(os.read(self.err, 4096), "master")
+        if ev & Poll.ERR_FLAGS:
+            self.poll.unregister(self._handle_err)
+            os.close(self.err)
+            self.err = None
+            self._maybe_wait()
+
+    def get_precommand(self):
+        return None
+
+    def run_command(self):
+        self.dying = False
+        self.proc = subprocess.Popen(
+            MASTER_CMD,
+            # ["master.sh"],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        self.out = self.proc.stdout.fileno()
+        self.err = self.proc.stderr.fileno()
+
+        nonblock(self.out)
+        nonblock(self.err)
+
+        self.poll.register(self.out, Poll.IN_FLAGS, self._handle_out)
+        self.poll.register(self.err, Poll.IN_FLAGS, self._handle_err)
+
+    def get_postcommand(self):
+        if self.postcommands_run == 1:
+            return None
+
+        self.postcommands_run += 1
+
+        atomic_op = ConnCheck("localhost", 8080, self.pipe_wr, "MASTER")
+        atomic_op.start()
+        return atomic_op
+
+    def running(self):
+        return self.proc is not None
+
+    def kill(self):
+        self.dying = True
+        self.proc.kill()
+
+class Poll:
+    IN_FLAGS = select.POLLIN | select.POLLPRI
+    ERR_FLAGS = select.POLLERR | select.POLLHUP | select.POLLNVAL
+
+    def __init__(self):
+        self.handlers = {}
+        self.fds = {}
+        self._poll = select.poll()
+
+    def register(self, fd, flags, handler):
+        self._poll.register(fd, flags)
+        self.handlers[fd] = handler
+        self.fds[handler] = fd
+
+    def unregister(self, handler):
+        fd = self.fds[handler]
+        self._poll.unregister(fd)
+        del self.fds[handler]
+        del self.handlers[fd]
+
+    def poll(self, *args, **kwargs):
+        ready = self._poll.poll()
+        for fd, ev in ready:
+            handler = self.handlers[fd]
+            handler(ev)
+
 class ConnCheck(threading.Thread):
+    """ConnCheck is an AtomicOperation."""
+
     def __init__(self, host, port, report_fd, success_msg):
         self.host = host
         self.port = port
@@ -97,6 +310,9 @@ class ConnCheck(threading.Thread):
         self.quit = False
 
         super().__init__()
+
+    def __str__(self):
+        return "connecting"
 
     def run(self):
         success = False
@@ -118,27 +334,25 @@ class ConnCheck(threading.Thread):
                 success = True
                 break
         finally:
-            if not self.quit:
-                os.write(self.report_fd, self.success_msg if success else b'FAIL')
+            os.write(self.report_fd, self.success_msg if success else b'FAIL')
+
+    def cancel(self):
+        self.quit = True
+
 
 class NDet:
     def __init__(self):
         self.quitting = False
 
-        self.master_proc = None
-        self.master_out = None
-        self.master_err = None
-        self.master_dying = False
+        # atomic_op is intermediate steps like calling `make` or connecting to a server
+        self.atomic_op = None
+        # the pipe is used by the atomic_op to pass messages to the poll loop
+        self.pipe_rd, self.pipe_wr = os.pipe2(os.O_CLOEXEC | os.O_NONBLOCK)
 
-        self.connector = None
-        self.conn_read, self.conn_write = os.pipe2(os.O_CLOEXEC | os.O_NONBLOCK)
+        self.poll = Poll()
 
-        self.in_poll_flags = select.POLLIN | select.POLLPRI
-        self.err_poll_flags = select.POLLERR | select.POLLHUP | select.POLLNVAL
-
-        self.poll = select.poll()
-        self.poll.register(sys.stdin.fileno(), self.in_poll_flags)
-        self.poll.register(self.conn_read, self.in_poll_flags)
+        self.poll.register(sys.stdin.fileno(), Poll.IN_FLAGS, self.handle_stdin)
+        self.poll.register(self.pipe_rd, Poll.IN_FLAGS, self.handle_pipe)
 
         self.state = States.DEAD
         self.target = States.DEAD
@@ -150,6 +364,9 @@ class NDet:
         self.active_stream = ""
 
         self.set_active_stream("ndet")
+
+        # Master process
+        self.master = Master(self.poll, self.log, self.pipe_wr, self.set_target, self.next_thing)
 
 
     def log(self, msg, stream="ndet"):
@@ -175,48 +392,68 @@ class NDet:
 
         self.active_stream = new_stream
 
-    def nonblock(self, fd):
-        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+    def advance_process(self, process):
+        """
+        Either:
+          - start the next precommand,
+          - start the real command (and maybe a postcommand), or
+          - start the next postcommand
+        """
+        # Is there another precommand?
+        atomic_op = process.get_precommand()
+        if atomic_op is not None:
+            self.atomic_op = atomic_op
+            return
 
-    def close_connector(self):
-        if self.connector is not None:
-            self.connector.quit = True
-            self.connector.join()
-            self.connector = None
+        if not process.running():
+            process.run_command()
 
-    def reset_connector(self, host, port, next_state):
-        self.close_connector()
-
-        self.connector = ConnCheck(host, port, self.conn_write, next_state)
-        self.connector.start()
+        atomic_op = process.get_postcommand()
+        if atomic_op is not None:
+            self.atomic_op = atomic_op
+            return
 
     def next_thing(self):
+        """
+        Should be called either when:
+          - a new transition is set
+          - an atomic operation completes
+          - a long-running process is closed
+        """
         # Nothing to do.
         if self.state == self.target:
             return
 
-        self.log(f"next thing: state = {self.state}, target = {self.target}\n")
-
-        if self.state == States.DEAD:
-            self.start_master()
-            return
-
-        elif self.state == States.MASTER:
-            if self.target == States.DEAD:
-                self.kill_master()
+        # Advance state?
+        if self.state < self.target:
+            # Wait for the atomic operation to finish.
+            if self.atomic_op is not None:
                 return
 
-        raise NotImplementedError()
+            if self.state == States.DEAD:
+                self.transition(States.MASTER)
+                self.advance_process(self.master)
+            else:
+                raise NotImplementedError()
 
+        # Regress state.
+        if self.state > self.target:
+            # Cancel any atomic operations first.
+            if self.atomic_op is not None:
+                self.atomic_op.cancel()
+                return
+
+            if self.state == States.MASTER:
+                if self.master.running():
+                    self.master.kill()
+                    return
+                self.transition(States.DEAD)
+            else:
+                raise NotImplementedError()
 
     def set_target(self, target):
         """For when you choose a new target state."""
         if target == self.target:
-            return
-
-        if self.state in {States.PRE_DB, States.PRE_HASURA, States.PRE_MASTER}:
-            # Wait till the intermediate state resolves
             return
 
         self.target = target
@@ -227,49 +464,7 @@ class NDet:
         """For when you arrive at a new state."""
         self.state = new_state
 
-        if self.state == States.DEAD and self.quitting:
-            sys.exit(0)
-
         self.next_thing()
-
-    def start_master(self):
-        self.master_dying = False
-        self.master_proc = subprocess.Popen(
-            MASTER_CMD,
-            # ["master.sh"],
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        self.master_out = self.master_proc.stdout.fileno()
-        self.master_err = self.master_proc.stderr.fileno()
-
-        self.nonblock(self.master_out)
-        self.nonblock(self.master_err)
-
-        self.poll.register(self.master_out, self.in_poll_flags)
-        self.poll.register(self.master_err, self.in_poll_flags)
-
-        self.state = States.PRE_MASTER
-        self.reset_connector("localhost", 8080, "MASTER")
-
-    def kill_master(self):
-        self.master_dying = True
-        self.master_proc.kill()
-        self.state = States.PRE_MASTER
-
-    def maybe_wait_master(self):
-        """wait() on master if both stdout and stderr are empty."""
-        if self.master_out is None and self.master_err is None:
-            ret = self.master_proc.wait()
-            self.log(f"master exited with {ret}\n")
-            self.log(f"NDET: master exited with {ret}\n", "master")
-            self.master_proc = None
-
-            self.close_connector()
-
-            self.transition(States.DEAD)
-            self.log(f"after transition, self.state is {self.state}\n")
 
     def get_cursor_pos(self):
         os.write(1, b'\x1b[6n')
@@ -296,38 +491,16 @@ class NDet:
         _, row, col = self.get_cursor_pos()
         self.place_cursor(1, 1)
         self.erase_line()
-        msg = f"state:{self.state} target:{self.target} stream:{self.active_stream}".encode("utf8")
+        state_msg = str(self.state) + (f"({self.atomic_op})" if self.atomic_op is not None else "")
+        msg = f"state:{state_msg} target:{self.target} stream:{self.active_stream}".encode("utf8")
         os.write(sys.stdout.fileno(), invert_colors(msg))
         self.place_cursor(row, col)
 
-    def loop(self):
-        while True:
-            self.print_bar()
-            ready = self.poll.poll()
-            for fd, ev in ready:
-                # console input
-                if fd == sys.stdin.fileno():
-                    self.handle_stdin(ev)
-
-                # connector pipe
-                elif fd == self.conn_read:
-                    self.handle_connector(ev)
-
-                # master fds
-                elif fd == self.master_out:
-                    self.handle_master_out(ev)
-                elif fd == self.master_err:
-                    self.handle_master_err(ev)
-
     def run(self):
-
         self.set_target(States.MASTER)
-
-        try:
-            self.loop()
-        finally:
-            if self.master_proc is not None:
-                raise NotImplementedError("emergency shut down doesn't work")
+        while not (self.quitting and self.state == States.DEAD):
+            self.print_bar()
+            self.poll.poll()
 
     def handle_stdin(self, ev):
         c = sys.stdin.read(1)
@@ -348,54 +521,21 @@ class NDet:
             self.set_target(States.MASTER)
             self.log("set target master!\n")
 
-    def handle_connector(self, ev):
-        self.connector.join()
-        self.connector = None
+    def handle_pipe(self, ev):
+        self.atomic_op.join()
+        self.atomic_op = None
 
-        if ev & self.in_poll_flags:
-            msg = os.read(self.conn_read, 4096).decode("utf8")
+        if ev & Poll.IN_FLAGS:
+            msg = os.read(self.pipe_rd, 4096).decode("utf8")
             if msg == "FAIL":
-                if self.state == States.PRE_MASTER:
-                    self.kill_master()
-                else:
-                    raise NotImplementedError()
-                return
+                # set the target state to be one less than wherever-we-are
+                self.target = max(self.state - 1, self.target)
 
-            msg = States(msg)
-            if self.state == States.PRE_MASTER and msg == States.MASTER:
-                self.transition(States.MASTER)
-            else:
-                raise NotImplementedError()
+            self.next_thing()
 
-        if ev & self.err_poll_flags:
+        if ev & Poll.ERR_FLAGS:
             # Just die.
             raise ValueError("pipe failed!")
-
-    def handle_master_out(self, ev):
-        if ev & self.in_poll_flags:
-            self.log(os.read(self.master_out, 4096), "master")
-        if ev & self.err_poll_flags:
-            self.poll.unregister(self.master_out)
-            os.close(self.master_out)
-            self.master_out = None
-            if self.master_dying:
-                self.maybe_wait_master()
-            else:
-                self.log(b'master stdout closed unexpectedly\n')
-                self.set_target(States.DEAD)
-
-    def handle_master_err(self, ev):
-        if ev & self.in_poll_flags:
-            self.log(os.read(self.master_err, 4096), "master")
-        if ev & self.err_poll_flags:
-            self.poll.unregister(self.master_err)
-            os.close(self.master_err)
-            self.master_err = None
-            if self.master_dying:
-                self.maybe_wait_master()
-            else:
-                self.log(b'master stderr closed unexpectedly\n')
-                self.set_target(States.DEAD)
 
 if __name__ == "__main__":
     with terminal_config():
