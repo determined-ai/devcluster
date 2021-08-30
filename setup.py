57c1e1271c1a94149688b64f5cd5e6cfaf9bf8bb
Ryan <rb@hpe.com>
2021-08-30 13:32:02 -0600

    chore: add a LICENSE (#6)

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setuptools.setup(
     name="devcluster",
     version="0.1",
     author="Determined AI",
-    author_email="rb@determined.ai",
+    author_email="hello@determined.ai",
     url="https://github.com/determined-ai/devcluster/",
     description="Developer tool for running the Determined cluster",
     python_requires=">=3.6",
     packages=["devcluster"],
     entry_points={"console_scripts": ["devcluster = devcluster.__main__:main"]},
+    license="Apache License 2.0",
 )
