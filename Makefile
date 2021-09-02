8a019e7af0f9fb3a7f376dca87b4eaf56fa565cf
Ryan <rb@hpe.com>
2021-09-01 20:21:01 -0600

    chore: prepare for publishing (#12)

--- Makefile
+++ Makefile
@@ -1,9 +1,30 @@
 all: check
 
+.PHONY: check
 check:
 	black --check devcluster
 	flake8 devcluster
 	mypy devcluster
 
+.PHONY: fmt
 fmt:
 	black devcluster
+
+.PHONY: clean
+clean:
+	rm -rf build/ dist/ *.egg-info
+
+.PHONY: build
+build:
+	$(MAKE) clean
+	python3 setup.py -q sdist bdist_wheel
+
+.PHONY: publish
+publish: build
+	twine check dist/*
+	twine upload --verbose --non-interactive dist/*
+
+.PHONY: test-publish
+test-publish: build
+	twine check dist/*
+	twine upload --verbose --non-interactive --repository-url https://test.pypi.org/legacy/ dist/*
--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+import pathlib
 import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+with (HERE / "devcluster" / "__init__.py").open() as f:
+    firstline = next(iter(f))
+    assert firstline.startswith("__version__ = ")
+    version = firstline.split('"')[1]
 
 setuptools.setup(
     name="devcluster",
-    version="0.1",
+    version=version,
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://github.com/determined-ai/devcluster/",
+    license="Apache License 2.0",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
     description="Developer tool for running the Determined cluster",
+    long_description=README,
+    long_description_content_type="text/markdown",
     python_requires=">=3.6",
     packages=["devcluster"],
     entry_points={"console_scripts": ["devcluster = devcluster.__main__:main"]},
-    license="Apache License 2.0",
     include_package_data=True,
     install_requires=[
         "appdirs",
