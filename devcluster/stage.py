dd5e1f067d8d3e75eb50571865f1705c714aaedb
Ryan <rb@hpe.com>
2022-02-09 13:37:36 -0700

    fix: deal with docker hangs on mac (#25)

--- devcluster/stage.py
+++ devcluster/stage.py
@@ -1,7 +1,9 @@
 import abc
 import os
+import selectors
 import signal
 import subprocess
+import time
 import typing
 
 import devcluster as dc
@@ -277,7 +279,7 @@ class DockerProcess(BaseProcess):
         self.container_id = ""
 
         # docker run --detach has to be an AtomicOperation because it is way too slow and causes
-        # the UI to hang.  This has far-reaching implications, since `running()` is not longer
+        # the UI to hang.  This has far-reaching implications, since `running()` is no longer
         # based on the main subprocess, and you may then have to `kill()` or `wait()` while the
         # main subprocess hasn't even been launched.
         self.docker_started = False
@@ -342,7 +344,7 @@ class DockerProcess(BaseProcess):
 
         self.process_tracker.report_pid_started(self.proc.pid, "docker container logs")
 
-    def docker_wait(self) -> int:
+    def docker_wait(self, timeout: typing.Optional[float] = None) -> int:
         # Wait for the container.
         self.docker_started = False
 
@@ -356,7 +358,32 @@ class DockerProcess(BaseProcess):
         )
         assert p.stdout
         assert p.stderr
-        err = p.stderr.read()
+
+        if timeout:
+            # Normally we only use docker_wait() when we already have reason to believe that the
+            # container is already dead (both stdout and stderr have closed).
+            # But there's a special case where if a stage is canceled before it is done with
+            # startup, we end up waiting for the startup to finish and kill the container an instant
+            # later.  Seemingly only on macs, the container seems to miss the early signal and
+            # docker_wait() will hang forever.  We introduce this wait-with-timeout as more of a
+            # workaround than anything else.  On Linux, the wait ought to be basically instant and
+            # the timeout case ought to behave almost identically to the non-timeout case.
+            try:
+                err = readall_with_timeout(p.stderr, timeout)
+            except TimeoutError:
+                # stop trying to `docker wait` and just nuke the container.
+                p.kill()
+                p.wait()
+                self.logger.log(
+                    f" ----- `docker wait` for {self.log_name()} took too long, force-removing the "
+                    "container -----\n",
+                    self.log_name(),
+                )
+                self.docker_rm(force=True)
+                return -1
+        else:
+            err = p.stderr.read()
+
         ret = p.wait()
         if ret != 0:
             self.logger.log(
@@ -369,13 +396,20 @@ class DockerProcess(BaseProcess):
                 f" ----- `docker wait` for {self.log_name()} exited with {ret} -----\n",
                 self.log_name(),
             )
-            # We don't know what to return
+            self.docker_rm(force=True)
             return -1
 
         docker_exit_val = int(p.stdout.read().strip())
 
-        # Remove the container
-        run_args = ["docker", "container", "rm", self.config.container_name]
+        self.docker_rm(force=False)
+
+        return docker_exit_val
+
+    def docker_rm(self, force: bool = False) -> None:
+        run_args = ["docker", "container", "rm"]
+        if force:
+            run_args += ["--force"]
+        run_args += [self.config.container_name]
 
         p = subprocess.Popen(
             run_args,
@@ -400,8 +434,6 @@ class DockerProcess(BaseProcess):
             )
         else:
             self.process_tracker.report_container_killed(self.container_id)
-
-        return docker_exit_val
 
     def wait(self) -> int:
         assert self.proc
@@ -443,8 +475,34 @@ class DockerProcess(BaseProcess):
 
         # If we got canceled, immediately wait on the docker container to exit; there won't be any
         # closing file descriptors to alert us that the container has closed.
-        # TODO: configure some async thing to wake up the poll loop via the report_fd so this
-        # doesn't block.
         if self.proc is None:
-            self.docker_wait()
+            self.docker_wait(timeout=0.5)
             self.state_machine.next_thing()
+
+
+def readall_with_timeout(f: typing.Any, timeout: float) -> bytes:
+    """Use selectors to read from a file descriptor with a timeout."""
+    dc.nonblock(f)
+    now = time.time()
+    deadline = now + timeout
+
+    buffer = b""
+    # TODO: convert the whole devcluster to use selectors instead of select.poll.
+    with selectors.DefaultSelector() as sel:
+        sel.register(f, selectors.EVENT_READ)
+        keep_going = True
+        while keep_going:
+            for _, events in sel.select(timeout=deadline - now):
+                if not (events & selectors.EVENT_READ):
+                    raise ValueError("error selecting on file object")
+                data = f.read(4096)
+                if not data:
+                    # Normal EOF.
+                    keep_going = False
+                    break
+                buffer += data
+            now = time.time()
+            if now >= deadline:
+                raise TimeoutError()
+
+    return buffer
